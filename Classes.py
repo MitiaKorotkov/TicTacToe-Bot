826e961c4eec7c397273c6c03783e505848487c8
MitiaKorotkov <55015177+MitiaKorotkov@users.noreply.github.com>
2019-12-08 02:09:43 +0300

    Начало работы
    Файл с классами основных объектов

---
+++ Classes.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+class Board:
+    """
+    Класс игровой доски
+    """
+    pass
+
+
+class Cross:
+    """
+    Класс крестика
+    """
+    pass
+
+
+class Dot:
+    """
+    Класс нолика
+    """
+    pass
+
+
+if __name__ == '__main__':
+    print('This module is not to run the program')
