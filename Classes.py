ef445bb00ae5981096c5b5e6369bff8671b4a594
SpirinYegor <55591739+SpirinYegor@users.noreply.github.com>
2019-12-10 17:34:13 +0300

    Fixed and added "Draw"

--- Classes.py
+++ Classes.py
@@ -5,21 +5,52 @@ class Board:
     """
     Класс игровой доски
     """
-    pass
+    def __init__(self, number_of_cells, size, x, y, color):
+        self.number_of_cells = number_of_cells
+        self.size = size
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def draw(self, surface):
+        for i in range(self.number_of_cells + 1):
+            pygame.draw.line(surface, self.color, (self.x + i * self.size / self.number_of_cells, self.y),
+                             (self.x + i * self.size / self.number_of_cells, self.y + self.size), 3)
+        for i in range(self.number_of_cells + 1):
+            pygame.draw.line(surface, self.color, (self.x, self.y + i * self.size / self.number_of_cells),
+                             (self.x + self.size, self.y + i * self.size / self.number_of_cells), 3)
+
 
 
 class Cross:
     """
     Класс крестика
     """
-    pass
+    def __init__(self, size, color):
+        self.size = size
+        self.x = 0
+        self.y = 0
+        self.color = color
+
+    def draw(self, surface):
+        pygame.draw.line(surface, self.color, (self.x - self.size, self.y - self.size),
+                         (self.x + self.size, self.y + self.size), 4)
+        pygame.draw.line(surface, self.color, (self.x - self.size, self.y + self.size),
+                         (self.x + self.size, self.y - self.size), 4)
 
 
 class Dot:
     """
     Класс нолика
     """
-    pass
+    def __init__(self, size, color):
+        self.size = size
+        self.x = 0
+        self.y = 0
+        self.color = color
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (int(self.x), int(self.y)), self.size, int(0.2 * self.size))
 
 
 if __name__ == '__main__':
--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,44 +1,111 @@
 import pygame
 import Classes
 
-number_of_cells = 5  # Количество клеток
-x_coord = 100  # Координата левого верхнего угла по оси Х
-y_coord = 100  # Координата левого верхнего угла по оси У
-win_trigger = 0  # Требуемое колличество фигур в ряду для победы
-board_size = 600  # Визуальный размер доски
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-chip_size = None  # Fixme что это
-screen = None  # Fixme что это
-clock = None  # Таймер
-data = None  # Массив данных о расположении фигур
-
 
 def win_check(array, x, y, obj):
+    global moves_quantity, win_trigger
     """
     Функция проверяет наличия победной позиции
 
     :param array: Массив положений крестиков и ноликов
     :param x: координата проверяемой клетки поля по оси Х
     :param y: координата проверяемой клетки поля по оси У
-    :param obj: Fixme Что это такое
+    :param obj: Тип проверяемой фишки (Крестик/Нолик)
     """
-    pass
+    draw_check = 1
+    win_diagonal1 = 0
+    win_vertical = 0
+    win_horizontal = 0
+    win_diagonal2 = 0
+    for I in range(-3, 4):
+        if array[x + I][y + I] == array[x + I - 1][y + I - 1] == array[x + I + 1][y + I + 1] == obj:
+            win_diagonal1 += 1
+    for I in range(-3, 4):
+        if array[x + I][y - I] == array[x + I - 1][y - I + 1] == array[x + I + 1][y - I - 1] == obj:
+            win_diagonal2 += 1
+    for I in range(-3, 4):
+        if array[x][y + I] == array[x][y + I - 1] == array[x][y + I + 1] == obj:
+            win_vertical += 1
+    for I in range(-3, 4):
+        if array[x + I][y] == array[x + I - 1][y] == array[x + I + 1][y] == obj:
+            win_horizontal += 1
+    if win_diagonal1 >= 3 or win_horizontal >= 3 or win_diagonal2 >= 3 or win_vertical >= 3:
+        win_trigger = 1
+        draw_check = 0
+        if obj == 1:
+            f = pygame.font.SysFont('serif', 50)
+            text = f.render('Crosses win!', 1, RED)
+            screen.blit(text, (800, 370))
+        if obj == -1:
+            f = pygame.font.SysFont('serif', 50)
+            text = f.render('Dots win!', 1, BLUE)
+            screen.blit(text, (800, 370))
+    moves_quantity += 1
+    if moves_quantity == number_of_cells * number_of_cells and draw_check == 1:
+        f = pygame.font.SysFont('serif', 50)
+        text = f.render('Draw!', 1, BLACK)
+        screen.blit(text, (800, 370))
 
 
 def start():
     """
     Главный цикл отрисовки
     """
-    pass
+    screen.fill((192, 192, 192))
+    board = Classes.Board(number_of_cells, board_size, x_coord, y_coord, BLACK)
+    check = 1
+    board.draw(screen)
+    while True:
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                exit()
+            if i.type == pygame.MOUSEBUTTONDOWN:
+                click_x = 10 + ((i.pos[0] - board.x) // (board_size / number_of_cells))
+                click_y = 10 + ((i.pos[1] - board.y) // (board_size / number_of_cells))
+                if i.button == 1 and 100 < i.pos[0] < (100 + board_size) and 100 < i.pos[1] < (100 + board_size) and \
+                        data[int(click_x)][int(click_y)] == 0 and win_trigger == 0:
+                    if check == 1:
+                        obj = Classes.Cross(chip_size, RED)
+                        data[int(click_x)][int(click_y)] = check
+                        check = -1
+                    else:
+                        obj = Classes.Dot(chip_size, BLUE)
+                        data[int(click_x)][int(click_y)] = check
+                        check = 1
+                    obj.x = (click_x - 10) * (board_size / number_of_cells) + (board_size / number_of_cells) / 2 + 100
+                    obj.y = (click_y - 10) * (board_size / number_of_cells) + (board_size / number_of_cells) / 2 + 100
+                    obj.draw(screen)
+                    win_check(data, int(click_x), int(click_y), -check)
+        pygame.display.update()
+        clock.tick(60)
 
 
 def main():
     """
     Функция координирующая работу пограммы в целом
     """
-    pass
+    global number_of_cells, board_size, screen, clock, data, \
+        win_trigger, x_coord, y_coord, BLACK, BLUE, RED, moves_quantity, chip_size
+    pygame.init()
+    number_of_cells = 20  # Количество клеток
+    x_coord = 100  # Координата левого верхнего угла по оси Х
+    y_coord = 100  # Координата левого верхнего угла по оси У
+    win_trigger = 0  # Проверка наличия на поле выигрышной комбинации
+    board_size = 600  # Визуальный размер доски
+    BLACK = (0, 0, 0)
+    BLUE = (0, 0, 255)
+    RED = (255, 0, 0)
+    moves_quantity = 0  # Число сделанных ходов
+    chip_size = int(board_size / number_of_cells / 2)  # Размер(радиус) фишки
+    screen = pygame.display.set_mode((1200, 780))  # Инициализация окна
+    clock = pygame.time.Clock()  # Таймер
+    data = []  # Массив данных о расположении фигур
+    for i in range(number_of_cells + 25):
+        small_data = []
+        for j in range(number_of_cells + 25):
+            small_data.append(0)
+        data.append(small_data)
+    start()
 
 
 if __name__ == '__main__':
